5c9162c95642370ef0767e6502caa46a874113e5
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 16:42:43 +0200

    Move enum to its own file

--- src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
+++ src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
@@ -2,39 +2,12 @@ package fr.gildedrose.domain
 
 class ItemUpdater(var item: Item) {
   fun updateQuality() {
-    if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+    if (item.name == "Sulfuras, Hand of Ragnaros") {
       return
     }
 
     item.sellIn = item.sellIn - 1
     item.increaseQualityBy(1)
     item = ItemAction.findItemByName(item.name).action(item)
-  }
-}
-
-enum class ItemAction(val itemName: String, val action: (item: Item) -> Item) {
-  AGED_BRIE("Aged Brie", { item ->
-    if (item.isSellInExpired()) item.increaseQualityBy(1)
-    item
-  }),
-  BACKSTAGE("Backstage passes to a TAFKAL80ETC concert", { item ->
-    when {
-      item.isSellInMedium() -> item.increaseQualityBy(1)
-      item.isSellInLow() -> item.increaseQualityBy(2)
-      item.isSellInExpired() -> item.decreaseQualityBy(item.quality)
-    }
-    item
-  }),
-  DEFAULT("default", { item ->
-    item.decreaseQualityBy(2)
-    if (item.isSellInExpired()) item.decreaseQualityBy(1)
-    item
-  });
-
-  companion object {
-    fun findItemByName(name: String): ItemAction {
-      return values()
-              .find { it.itemName.toLowerCase() == name.toLowerCase() } ?: ItemAction.DEFAULT
-    }
   }
 }
