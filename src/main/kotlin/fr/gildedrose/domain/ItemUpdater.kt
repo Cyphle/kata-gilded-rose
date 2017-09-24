09626799bcb5e38af267451c737ba9bebaa7da54
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 16:08:53 +0200

    Replace if by when

--- src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
+++ src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
@@ -5,48 +5,51 @@ class ItemUpdater(val item: Item) {
     if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
       return
     }
+
     item.sellIn = item.sellIn - 1
     increaseQualityBy(1)
 
-    if (item.name.equals("Aged Brie")) {
-      if (item.sellIn < 0) {
+    when {
+      item.name == "Aged Brie" -> if (isSellInExpired()) {
         increaseQualityBy(1)
       }
-    } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      if (item.sellIn < 10) {
-        increaseQualityBy(1)
+      item.name == "Backstage passes to a TAFKAL80ETC concert" -> when {
+        isSellInMedium() -> increaseQualityBy(1)
+        isSellInLow() -> increaseQualityBy(2)
+        isSellInExpired() -> decreaseQuality(item.quality)
       }
-
-      if (item.sellIn < 5) {
-        increaseQualityBy(1)
-      }
-
-      if (item.sellIn < 0) {
-        decreaseQuality(item.quality)
-      }
-
-    } else {
-      decreaseQuality(1)
-      decreaseQuality(1)
-
-      if (item.sellIn < 0) {
-        decreaseQuality(1)
+      else -> {
+        decreaseQuality(2)
+        if (isSellInExpired()) {
+          decreaseQuality(1)
+        }
       }
     }
   }
 
+  private fun isSellInMedium() = item.sellIn < 10 && item.sellIn >= 5
+
+  private fun isSellInLow() = item.sellIn < 5 && item.sellIn >= 0
+
+  private fun isSellInExpired() = item.sellIn < 0
+
   private fun decreaseQuality(value: Int) {
-    if (item.quality > 0) {
-      item.quality = item.quality - value
+    var decreaseLevel = value
+    while (decreaseLevel > 0) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
+      }
+      --decreaseLevel
     }
   }
 
   private fun increaseQualityBy(value: Int) {
-    if (item.quality < 50) {
-      item.quality = item.quality + value
+    var increaseLevel = value
+    while (increaseLevel > 0) {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+      }
+      --increaseLevel
     }
   }
-}
-
-enum class SellInCeil(val ceil: Int, val qualityModification: Int) {
 }
