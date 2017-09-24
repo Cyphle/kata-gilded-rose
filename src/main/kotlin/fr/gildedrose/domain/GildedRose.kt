02da619ed619c921cfb5f30db707a825c4607719
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 14:13:52 +0200

    Add sulfuras guard because it never do anything when it is sulfuras

--- src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
+++ src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
@@ -8,6 +8,10 @@ class GildedRose(var items: Array<Item>) {
   }
 
   private fun updateQuality(i: Int) {
+    if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+      return
+    }
+
     if (items[i].name.equals("Aged Brie")) {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
@@ -46,17 +50,11 @@ class GildedRose(var items: Array<Item>) {
       }
     } else {
       if (items[i].quality > 0) {
-        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-          items[i].quality = items[i].quality - 1
-        }
+        items[i].quality = items[i].quality - 1
       }
     }
 
-    if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-    } else {
-      items[i].sellIn = items[i].sellIn - 1
-    }
+    items[i].sellIn = items[i].sellIn - 1
 
     if (items[i].sellIn < 0) {
       if (items[i].name.equals("Aged Brie")) {
@@ -68,9 +66,6 @@ class GildedRose(var items: Array<Item>) {
           items[i].quality = items[i].quality - items[i].quality
         } else {
           if (items[i].quality > 0) {
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-              return
-            }
             items[i].quality = items[i].quality - 1
           }
         }
