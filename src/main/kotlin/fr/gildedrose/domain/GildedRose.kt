9e89597b602e92fa7355e52d2c5576f7b1f05032
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 14:12:40 +0200

    Split if with multiple conditions to multiple if

--- src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
+++ src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
@@ -1,56 +1,77 @@
 package fr.gildedrose.domain
 
 class GildedRose(var items: Array<Item>) {
-
   fun updateQuality() {
-    for (i in items.indices) {
-      if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+    items.indices.forEach { i ->
+      updateQuality(i)
+    }
+  }
+
+  private fun updateQuality(i: Int) {
+    if (items[i].name.equals("Aged Brie")) {
+      if (items[i].quality < 50) {
+        items[i].quality = items[i].quality + 1
+
+        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].sellIn < 11) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+
+          if (items[i].sellIn < 6) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+        }
+      }
+    } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (items[i].quality < 50) {
+        items[i].quality = items[i].quality + 1
+
+        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (items[i].sellIn < 11) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+
+          if (items[i].sellIn < 6) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1
+            }
+          }
+        }
+      }
+    } else {
+      if (items[i].quality > 0) {
+        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else {
+          items[i].quality = items[i].quality - 1
+        }
+      }
+    }
+
+    if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+    } else {
+      items[i].sellIn = items[i].sellIn - 1
+    }
+
+    if (items[i].sellIn < 0) {
+      if (items[i].name.equals("Aged Brie")) {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
-
-          if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].sellIn < 11) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-          }
         }
       } else {
-        if (items[i].quality > 0) {
-          if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-          } else {
-            items[i].quality = items[i].quality - 1
-          }
-        }
-      }
-
-      if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-      } else {
-        items[i].sellIn = items[i].sellIn - 1
-      }
-
-      if (items[i].sellIn < 0) {
-        if (items[i].name.equals("Aged Brie")) {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
-          }
+        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          items[i].quality = items[i].quality - items[i].quality
         } else {
-          if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            items[i].quality = items[i].quality - items[i].quality
-          } else {
-            if (items[i].quality > 0) {
-              if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                continue
-              }
-              items[i].quality = items[i].quality - 1
+          if (items[i].quality > 0) {
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+              return
             }
+            items[i].quality = items[i].quality - 1
           }
         }
       }
