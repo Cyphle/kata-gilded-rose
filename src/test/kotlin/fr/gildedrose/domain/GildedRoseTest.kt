6d9c730e85b5a41bdc6469169a248208aa1be706
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 14:07:31 +0200

    Test update quality once

--- src/test/kotlin/fr/gildedrose/domain/GildedRoseTest.kt
+++ src/test/kotlin/fr/gildedrose/domain/GildedRoseTest.kt
@@ -1,13 +1,34 @@
 package fr.gildedrose.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert.*
 import org.junit.Test
 
 class GildedRoseTest {
-  @Test fun foo() {
-    val items = arrayOf<Item>(Item("foo", 0, 0))
+  private val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+          Item("Aged Brie", 2, 0), //
+          Item("Elixir of the Mongoose", 5, 7), //
+          Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+          Item("Sulfuras, Hand of Ragnaros", -1, 80),
+          Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+          Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+          Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+          // this conjured item does not work properly yet
+          Item("Conjured Mana Cake", 3, 6))
+
+  @Test
+  fun `should update quality once`() {
     val app = GildedRose(items)
-    app.updateQuality()
-    assertEquals("fixme", app.items[0].name)
+    assertThat(app.items).isEqualTo(arrayOf(
+            Item("+5 Dexterity Vest", 10, 20),
+            Item("Aged Brie", 2, 0),
+            Item("Elixir of the Mongoose", 5, 7),
+            Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            Item("Conjured Mana Cake", 3, 6)
+    ))
   }
 }
