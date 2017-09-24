99a33e2345667246b8613ba59a4994241abd6de9
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 16:15:04 +0200

    Move decrease and increase method to item

--- src/main/kotlin/fr/gildedrose/domain/Item.kt
+++ src/main/kotlin/fr/gildedrose/domain/Item.kt
@@ -1,3 +1,23 @@
 package fr.gildedrose.domain
 
-data class Item(var name: String, var sellIn: Int, var quality: Int)
+data class Item(var name: String, var sellIn: Int, var quality: Int) {
+  fun decreaseQualityBy(value: Int) {
+    var decreaseLevel = value
+    while (decreaseLevel > 0) {
+      if (quality > 0) {
+        quality = quality - 1
+      }
+      --decreaseLevel
+    }
+  }
+
+  fun increaseQualityBy(value: Int) {
+    var increaseLevel = value
+    while (increaseLevel > 0) {
+      if (quality < 50) {
+        quality = quality + 1
+      }
+      --increaseLevel
+    }
+  }
+}
--- src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
+++ src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
@@ -7,21 +7,21 @@ class ItemUpdater(val item: Item) {
     }
 
     item.sellIn = item.sellIn - 1
-    increaseQualityBy(1)
+    item.increaseQualityBy(1)
 
     when {
       item.name == "Aged Brie" -> if (isSellInExpired()) {
-        increaseQualityBy(1)
+        item.increaseQualityBy(1)
       }
       item.name == "Backstage passes to a TAFKAL80ETC concert" -> when {
-        isSellInMedium() -> increaseQualityBy(1)
-        isSellInLow() -> increaseQualityBy(2)
-        isSellInExpired() -> decreaseQuality(item.quality)
+        isSellInMedium() -> item.increaseQualityBy(1)
+        isSellInLow() -> item.increaseQualityBy(2)
+        isSellInExpired() -> item.decreaseQualityBy(item.quality)
       }
       else -> {
-        decreaseQuality(2)
+        item.decreaseQualityBy(2)
         if (isSellInExpired()) {
-          decreaseQuality(1)
+          item.decreaseQualityBy(1)
         }
       }
     }
@@ -32,24 +32,4 @@ class ItemUpdater(val item: Item) {
   private fun isSellInLow() = item.sellIn < 5 && item.sellIn >= 0
 
   private fun isSellInExpired() = item.sellIn < 0
-
-  private fun decreaseQuality(value: Int) {
-    var decreaseLevel = value
-    while (decreaseLevel > 0) {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1
-      }
-      --decreaseLevel
-    }
-  }
-
-  private fun increaseQualityBy(value: Int) {
-    var increaseLevel = value
-    while (increaseLevel > 0) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-      }
-      --increaseLevel
-    }
-  }
 }
