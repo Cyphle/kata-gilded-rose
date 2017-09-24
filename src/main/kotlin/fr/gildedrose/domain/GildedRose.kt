9518cbd2fbaa45fbf3d11e042149c62f772f1d8c
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-24 15:36:03 +0200

    Remove parameter of methods

--- src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
+++ src/main/kotlin/fr/gildedrose/domain/GildedRose.kt
@@ -2,6 +2,6 @@ package fr.gildedrose.domain
 
 class GildedRose(var items: Array<Item>) {
   fun updateQuality() {
-    items.forEach { ItemUpdater(it).updateQuality(it) }
+    items.forEach { ItemUpdater(it).updateQuality() }
   }
 }
--- src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
+++ src/main/kotlin/fr/gildedrose/domain/ItemUpdater.kt
@@ -1,48 +1,48 @@
 package fr.gildedrose.domain
 
 class ItemUpdater(val item: Item) {
-  fun updateQuality(item: Item) {
+  fun updateQuality() {
     if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
       return
     }
     item.sellIn = item.sellIn - 1
 
     if (item.name.equals("Aged Brie")) {
-      increaseQuality(item)
+      increaseQuality()
 
       if (item.sellIn < 0) {
-        increaseQuality(item)
+        increaseQuality()
       }
     } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      increaseQuality(item)
+      increaseQuality()
 
       if (item.sellIn < 10) {
-        increaseQuality(item)
+        increaseQuality()
       }
 
       if (item.sellIn < 5) {
-        increaseQuality(item)
+        increaseQuality()
       }
 
       if (item.sellIn < 0) {
         item.quality = 0
       }
     } else {
-      decreaseQuality(item)
+      decreaseQuality()
 
       if (item.sellIn < 0) {
-        decreaseQuality(item)
+        decreaseQuality()
       }
     }
   }
 
-  private fun decreaseQuality(item: Item) {
+  private fun decreaseQuality() {
     if (item.quality > 0) {
       item.quality = item.quality - 1
     }
   }
 
-  private fun increaseQuality(item: Item) {
+  private fun increaseQuality() {
     if (item.quality < 50) {
       item.quality = item.quality + 1
     }
